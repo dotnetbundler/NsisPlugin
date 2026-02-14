d6afd79d4e9b07d27bc4c15d4b6c1f15a11c988a
Lin <linlccc@163.com>
2026-02-15 03:10:39 +0800

    fix: 修复自动获取 ModuleHandle 问题，后期通过源生成器设置

--- src/PluginApi.cs
+++ src/PluginApi.cs
@@ -1,11 +1,10 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿#pragma warning disable CS8618
 
 namespace NsisPlugin;
 
 public static class PluginApi
 {
-    public static IntPtr ModuleHandle { get; private set; }
+    public static IntPtr ModuleHandle { get; set; }
 
     [field: ThreadStatic] public static IntPtr HwndParent { get; private set; }
 
@@ -18,17 +17,6 @@ public static class PluginApi
     [field: ThreadStatic] public static ExtraParameters ExtraParameters { get; private set; }
 
     public static int MaxStringBytes => StringSize * PluginEncoding.CharSize;
-
-    static PluginApi()
-    {
-        Variables = null!;
-        StackTop = null!;
-        ExtraParameters = null!;
-        ModuleHandle = GetModuleHandle($"{Assembly.GetExecutingAssembly().GetName().Name}.dll");
-
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GetModuleHandle(string lpModuleName);
-    }
 
     public static void Init(IntPtr hwndParent, int stringSize, IntPtr variables, IntPtr stacktop, IntPtr extraParameters)
     {
